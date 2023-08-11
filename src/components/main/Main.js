0f027a8e708c91ee09de3b68478b1de8c9c91207
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2023-08-11 03:25:32 +0100

    Set up CSS Layout for the Web app

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,7 +2,90 @@ import React from 'react'
 
 function Main() {
   return (
-    <div>Main</div>
+    <>
+      <main>
+        <section className="hero-container">
+          <article className="hero-content">
+            <div className="text-container">
+              {/* <div>
+                <h1>Little Lemon</h1>
+                <h3>Chicago</h3>
+              </div>
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Rerum fugiat fuga quis atque vero, 
+                suscipit totam quisquam ullam odit 
+                maiores nostrum molestiae explicabo 
+                dolore ut provident commodi qui earum incidunt.
+              </div>
+              <button>
+                Reserve a table
+              </button> */}
+            </div>
+
+            <div className='image-container'>
+              <img src="#" alt="#" />
+            </div>
+          </article>
+        </section>
+        <section className="highlights-container">
+          <article className="highlights-content">
+            <div className="heading">
+              <h1>This Weekly Specials!</h1>
+              <button>Online Menu</button>
+            </div>
+
+            <div className="card-container">
+              <div className="card">
+                <div className="image-card">
+                  <img src="#" alt="#" />
+                </div>
+                <div className="card-text"></div>
+              </div>
+
+              <div className="card">
+                <div className="image-card">
+                  <img src="#" alt="#" />
+                </div>
+                <div className="card-text"></div>
+              </div>
+
+              <div className="card">
+                <div className="image-card">
+                  <img src="#" alt="#" />
+                </div>
+                <div className="card-text"></div>
+              </div>
+            </div>
+          </article>
+        </section>
+        <section className="testimonials"></section>
+        <section className="about">
+        <article className="hero-content">
+            <div className="text-container">
+              {/* <div>
+                <h1>Little Lemon</h1>
+                <h3>Chicago</h3>
+              </div>
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Rerum fugiat fuga quis atque vero, 
+                suscipit totam quisquam ullam odit 
+                maiores nostrum molestiae explicabo 
+                dolore ut provident commodi qui earum incidunt.
+              </div>
+              <button>
+                Reserve a table
+              </button> */}
+            </div>
+
+            <div className='image-container'>
+              <img src="#" alt="#" />
+            </div>
+          </article>
+        </section>
+      </main>
+    </>
   )
 }
 
