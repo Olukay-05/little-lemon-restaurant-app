18659620da87e12f037eee26519bbaccb1e1d443
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2023-08-18 23:33:42 +0100

    Added Table Booking State and Also Lifted State to Parent Component

--- src/pages/bookingPage/BookingPage.js
+++ src/pages/bookingPage/BookingPage.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState, useReducer } from 'react'
 
 import BookingForm from '../../components/form/BookingForm'
 
 function BookingPage() {
+
+  const [availableTimes, dispatch] = useReducer(updateTimesReducer, initializeTimes());
+
+  // Create a function to update available times based on the selected date
+  function updateTimesReducer(state, action) {
+    if (action.type === 'UPDATE_TIMES') {
+      // Here you can implement logic to update available times based on the selected date
+      // For now, we'll return the same available times
+      return initializeTimes();
+    }
+    return state;
+  }
+
+  // Create a function to initialize the available times
+  function initializeTimes() {
+    return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+  }
   return (
     <div>
-      <BookingForm />
+      <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
     </div>
   )
 }
