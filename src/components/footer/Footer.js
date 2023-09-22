c94ccd0aa716ccaa2c2323a2dcd8ec17a5ce7436
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2023-09-22 23:21:34 +0100

    Mobile Responsiveness - Small Screens

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from '../nav/Nav'
+import footerLogo from '../../assets/logos/footer-logo.svg'
 
 function Footer() {
   const contact = {
@@ -12,38 +13,33 @@ function Footer() {
   }
   return (
     <footer>
-      <div>
-        <img src="#" alt="#" />
-      </div>
-      <section className="footer-nav">
-        <h2>Doormart Navigation</h2>
-        <Nav 
-          home="Home"
-          about="About"
-          menu="Menu"
-          reservations="Reservations"
-          orderOnline="Order Online"
-          login="Login"
-          className='side-margin'
-        />
+      <section className='footer-img'>
+        <img src={ footerLogo } alt="Little lemon" className='footerLogo' />
+      </section>
+      <section className='footer-nav'>
+        <h2 className='footer-text'>Doormart Navigation</h2>
+        <a href="#" className='footer-text'>Home</a>
+        <a href="#" className='footer-text'>About</a>
+        <a href="#" className='footer-text'>Menu</a>
+        <a href="#" className='footer-text'>Reservations</a>
+        <a href="#" className='footer-text'>Order Online</a>
       </section>
    
-
-      <section className="footer-nav two">
-        <h2>Contact</h2>
-        <ul className='side-margin'>
-          <li> {contact.address} </li>
-          <li> {contact.phoneNumber} </li>
-          <li> {contact.email} </li>
+      <section>
+        <h2 className='footer-text'>Contact</h2>
+        <ul >
+          <li className='footer-text'> {contact.address} </li>
+          <li className='footer-text'> {contact.phoneNumber} </li>
+          <li className='footer-text'> {contact.email} </li>
         </ul>
       </section>
 
-      <section className="footer-nav three">
-        <h2>Social Media Links</h2>
-        <ul className='side-margin'>
-          <li> {contact.instagram} </li>
-          <li> {contact.twitter} </li>
-          <li> {contact.linkedin} </li>
+      <section>
+        <h2 className='footer-text'>Social Media Links</h2>
+        <ul>
+          <li className='footer-text'> {contact.instagram} </li>
+          <li className='footer-text'> {contact.twitter} </li>
+          <li className='footer-text'> {contact.linkedin} </li>
         </ul>
       </section>
     </footer>
